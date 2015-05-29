2049c245ad63bee923cbcd51e61cef3511491131
Breno Grillo <brenogrillo@gmail.com>
2015-05-29 18:34:43 -0300

    Adicionando função de edição de tarefa

--- app/edit-modal.php
+++ app/edit-modal.php
@@ -1,13 +1,14 @@
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#newTodoModal">
-  Cadastrar
+<button type="button" class="btn btn-primary"
+  data-toggle="modal" data-target="#editTodoModal<?=$id?>">
+  Editar
 </button>
 
-<div id="newTodoModal" class="modal fade bs-example-modal-sm"
+<div id="editTodoModal<?=$id?>" class="modal fade bs-example-modal-sm"
      tabindex="-1"
      role="dialog"
      aria-labelledby="mySmallModalLabel"
      aria-hidden="true">
-  <div class="modal-dialog modal-sm">
+  <div id="editTodo" class="modal-dialog modal-sm">
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
@@ -15,19 +16,24 @@
         </button>
         <h4 class="modal-title" id="myModalLabel">Cadastro de Tarefa</h4>
       </div>
-      <form id="newTodo" action="./" method="post">
+      <form id="editTodo" action="./" method="post">
         <div class="modal-body">
+          <input type="hidden" name="todo_id" value="<?=$id?>">
+          <?php
+            $checked = 'checked';
+          ?>
           <div class="form-group">
             <label for="descricao">Descrição:</label>
-            <input class="form-control" type="text" name="descricao"/>
+            <input class="form-control" type="text" name="todo_descricao"
+              value="<?=$todo["descricao"]?>"/>
           </div>
 
           <div class="form-group">
             <label for="concluida">Concluida:</label>
-            <i class="glyphicon glyphicon-ok text-success"></i>
-            <input type="radio" name="concluida" value="true"/>
-            <i class="glyphicon glyphicon-remove text-danger"></i>
-            <input type="radio" name="concluida" value="false" checked/>
+            Sim
+            <input type="radio" name="todo_concluida" value="true" <?php checkIfTrue($todo["concluida"]); ?>/>
+            Não
+            <input type="radio" name="todo_concluida" value="false" <?php checkIfFalse($todo["concluida"]); ?>/>
           </div>
         </div>
         <div class="modal-footer">
--- app/index.php
+++ app/index.php
@@ -2,8 +2,12 @@
   if (session_id() === "") { session_start(); }
   include_once "reload_session.php";
 
-  function haveTodo(){
-    return (isset($_POST["descricao"]) and isset($_POST["concluida"]));
+  function newTodo(){
+    return (
+      isset($_POST["descricao"]) and
+      isset($_POST["concluida"]) and
+      !isset($_POST["id"])
+    );
   }
 
   function addTodo(){
@@ -19,8 +23,51 @@
     }
   }
 
-  if(haveTodo()){
-      addTodo();
+  function hasTodo(){
+
+    if(!isset($_POST["todo_id"])){
+      return false;
+    }
+
+    $todo_id = $_POST["todo_id"];
+    return isset($_SESSION["Todos"][$todo_id]);
+  }
+
+  function editTodo($todo = null){
+    if(!$todo){
+      return false;
+    }
+    $currentTodo = &$_SESSION["Todos"][$todo["id"]];
+    return setTodo($currentTodo, $todo);
+  }
+
+  function setTodo(&$currentTodo, $todo){
+    $operation = ($currentTodo["descricao"] = $todo["descricao"]);
+    $operation = $operation and ($currentTodo["concluida"] = $todo["concluida"]);
+    return $operation;
+  }
+
+  function checkIfTrue($check){
+    if($check == 'true'){
+      echo 'checked';
+    }
+  }
+
+  function checkIfFalse($check){
+    if($check == 'false'){
+      echo 'checked';
+    }
+  }
+
+  if(newTodo()){
+    addTodo();
+  }else if(hasTodo()){
+    $todo = [
+        "id" => $_POST["todo_id"],
+        "descricao" => $_POST["todo_descricao"],
+        "concluida" => $_POST["todo_concluida"]
+    ];
+    editTodo($todo);
   }
 
 ?>
@@ -34,17 +81,15 @@
     <link rel="stylesheet" type="text/css" href="./css/bootstrap-theme.css" charset="utf-8">
     <link rel="stylesheet" type="text/css" href="./css/bootstrap.css" charset="utf-8">
     <link rel="stylesheet" type="text/css" href="./css/custom.css" charset="utf-8">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-    <script type="text/javascript" src="./js/bootstrap.js"></script>
-    <script type="text/javascript" src="./js/2do.js"></script>
+
   </head>
   <body>
     <header>
     </header>
-    <div class="row">
-      <h1 class="text-center">Bem vindo ao 2-do</h1>
-    </div>
     <div id="content" class="container col-sm-8 col-sm-offset-2">
+      <div class="row jumbotron">
+        <h1 class="text-center">Bem vindo ao 2-do</h1>
+      </div>
       <div class="row">
         <div class="pull-left">
           <form class="" action="./" method="post">
@@ -62,11 +107,14 @@
             <th class="col-sm-1">
               Id
             </th>
-            <th class="col-sm-8">
+            <th class="col-sm-6">
               Tarefa
             </th>
-            <th>
+            <th class="col-sm-2">
               Concluída?
+            </th>
+            <th>
+              Ações
             </th>
           </thead>
           <tbody>
@@ -90,6 +138,10 @@
                         endif;
                       ?>
                     </td>
+                    <td>
+                      <!-- Editar -->
+                      <?php include "./edit-modal.php" ?>
+                    </td>
                   </tr>
               <?php
                 }
@@ -103,5 +155,9 @@
     <footer>
 
     </footer>
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script type="text/javascript" src="./js/bootstrap.js"></script>
+    <script type="text/javascript" src="./js/2do.js"></script>
   </body>
 </html>
