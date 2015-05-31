c4a79f27a12dc572dd1fee33384fd1dd08a62e8c
Breno Grillo <brenogrillo@gmail.com>
2015-05-31 19:34:39 -0300

    Adding heroku database

--- functions.php
+++ functions.php
@@ -1,6 +1,27 @@
 <?php
   function getConnection(){
     try{
+      if (isset(getenv("CLEARDB_DATABASE_URL"))) {
+        $herokuDbUrl = getenv("CLEARDB_DATABASE_URL");
+        $url = parse_url($herokuDbUrl);
+
+        $server = $url["host"];
+        $username = $url["user"];
+        $password = $url["pass"];
+        $db = substr($url["path"], 1);
+
+        $pdo = new PDO(
+          $server . ';' . $db,
+          $username,
+          $password,
+        );
+
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        return $pdo;
+      }
+
+      // If is localhost
       $pdo = new PDO(
         'mysql:host=localhost;dbname=2do',
         'root',
@@ -8,6 +29,7 @@
       );
       $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       return $pdo;
+      
     }catch (PDOException $e){
       echo 'Error: ' . $e->getMessage();
       die('Can\'t connect to database');
